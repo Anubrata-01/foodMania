4fe6336eeece8b10156f4b2c06cc6609c3efd680
Anubrata-01 <anubratachanda36@gmail.com>
2024-08-24 17:12:49 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const dotenv = require('dotenv');
 const connectToDB = require('./database/db');
 const saveAddress = require("./routes/saveAddress");
 const getAddress = require("./routes/getUserAddress");
@@ -8,23 +9,18 @@ const checkoutSession = require("./routes/checkOutSession");
 const paymentSuccess = require("./routes/paymentSuccess");
 const orderDetails = require("./routes/getOrderDetails");
 const updateAddress = require("./routes/updateAddress");
+dotenv.config();
 app.use(express.json());
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://foodmaniaclient.onrender.com"
-// ];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
-app.use(cors({
-  origin: "https://foodmaniaclient.onrender.com"
-}));
+
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://foodmaniaclient.onrender.com'
+    : 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));;
 
 const PORT = process.env.PORT || 7000;
 
